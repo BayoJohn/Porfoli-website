fcb643a4e970b1ff18e44490c2a90a2c40d937af
Your Name <ogundelebayo8@gmail.com>
2026-03-13 07:15:27 +0100

    fix gitea token in manifest update script

--- update-manifest.sh
+++ update-manifest.sh
@@ -5,4 +5,4 @@ git clone http://172.17.0.1:3001/Bayo/Porfolio-website.git /tmp/repo
 sed -i "s|image:.*portfolio:.*|image: 10.0.0.2/library/portfolio:build-$1|g" /tmp/repo/k8s/deployment.yaml
 git -C /tmp/repo add k8s/deployment.yaml
 git -C /tmp/repo commit -m "ci update image to build-$1 [skip ci]"
-git -C /tmp/repo push http://Bayo:$2@172.17.0.1:3001/Bayo/Porfolio-website.git
+git -C /tmp/repo push http://Bayo:$GITEA_TOKEN@172.17.0.1:3001/Bayo/Porfolio-website.git
