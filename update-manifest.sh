7aad303b7cdbcc006bdbae53818d999365e35332
Your Name <ogundelebayo8@gmail.com>
2026-03-13 07:21:48 +0100

    debug token

--- update-manifest.sh
+++ update-manifest.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+echo "Token length: $(echo $GITEA_TOKEN | wc -c)"
 git config --global user.email "drone@ci.local"
 git config --global user.name "Drone CI"
 git clone http://172.17.0.1:3001/Bayo/Porfolio-website.git /tmp/repo
 sed -i "s|image:.*portfolio:.*|image: 10.0.0.2/library/portfolio:build-$1|g" /tmp/repo/k8s/deployment.yaml
 git -C /tmp/repo add k8s/deployment.yaml
 git -C /tmp/repo commit -m "ci update image to build-$1 [skip ci]"
-git -C /tmp/repo push http://Bayo:$GITEA_TOKEN@172.17.0.1:3001/Bayo/Porfolio-website.git
+git -C /tmp/repo push http://Bayo:$\{GITEA_TOKEN\}@172.17.0.1:3001/Bayo/Porfolio-website.git
